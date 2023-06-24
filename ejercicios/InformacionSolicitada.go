774d66c244592395a5b7ddb6e1235735d4576e15
FlorenciaChiappetta <111155028+FlorenciaChiappetta@users.noreply.github.com>
2023-06-24 15:10:59 -0300

    .

--- ejercicios/InformacionSolicitada.go
+++ ejercicios/InformacionSolicitada.go
@@ -4,6 +4,56 @@ import (
 	"guia6/dictionary"
 )
 
-func InformacionSolicitada(entrada dictionary.Dictionary[string, []string]) *dictionary.Dictionary[string, []string]{
-	panic("Not implemented")
+// 4. Tenemos que ayudar a los docentes a preparar la información solicitada por el
+// Departamento de Alumnos.
+// Cuando toman asistencia anotan los presentes por
+// día de clase, pero ahora desde Alumnos se les pide que envíen la información
+// de asistencia por alumno.  Por ejemplo, si la
+// lista que se recibe es:
+// {“Mie 10”: [“Ana", "Pedro"], “Vie 12”:, [“Ana", "Luz”], “Mie 17”:, [“Luz”, "Pedro"]}
+// el resultado debe ser
+// {"Ana":["Mie 10", "Vie 12"], "Luz": ["Vie 12", "Mie 17"], "Pedro": ["Mie 10", "Mie 17"]}
+func InformacionSolicitada(entrada dictionary.Dictionary[string, []string]) *dictionary.Dictionary[string, []string] {
+	dict := dictionary.NewDictionary[string, []string]()
+
+	for _, dia := range entrada.GetKeys() {
+
+		for _, nombres := range entrada.Get(dia) {
+			arreglo := []string{}
+			if dict.Contains(nombres) {
+				arreglo = dict.Get(dia)
+			}
+			arreglo = append(arreglo, dia)
+			dict.Put(nombres, arreglo)
+		}
+	}
+	return &dict
 }
+
+// func Ejercicio2 (tareas[]Tarea)
+// // ordenar las tareas por tiempo usando burbujeo
+
+// 	for i:=0; i < len(tareas)-1;i++{
+// 		for j:=0; j < len(tareas)-1;j++{
+
+// 	if tareas[j].Tiempo> tareas[j+1].Tiempo{
+
+// 	tareas[j],tareas[j+1] = tareas[j+1],tareas[j]
+// 		}
+// 	}
+// }
+// func Materias (materias dictionary.Dictionary[string,[]string]) dictionary.Dictionary.NewDictionary[string,[]string]()
+
+// for _, apellido := range materias.GetKeys(){
+// for _, alumno := range materias.Get(apellido){
+
+// arreglo := []string{}
+// if alumnos.Contains(alumno){
+// arreglo = alumnos.Get(alumno)
+// }
+// arreglo = append(arreglo,apellido)
+// alumnos.Put(alumno,apellido)
+// }
+// }
+// return alumnos
+// }
--- main.go
+++ main.go
@@ -2,68 +2,80 @@ package main
 
 import (
 	"fmt"
-	"guia6/set"
 	"guia6/dictionary"
-	"guia6/linkedlist"
+	"guia6/ejercicios"
 )
 
 func main() {
-	d := dictionary.NewDictionary[string, int]()
-	d.Put("Leo", 60)
-	d.Put("Leo", 61)
-	d.Put("Fabi", 36)
-	d.Put("Leo", 60)
-	d.Put("Fede", 36)
-	d.Put("Vale", 40)
-	d.Put("Leo", 50)
-	fmt.Println("Clave: valor en el diccionario (String, Int)")
-	for _, key := range d.GetKeys() {
-		fmt.Printf("%v:\t%d\n",key, d.Get(key))
-	}
-	fmt.Println("--------------------")
-	d.Remove("Fede")
-	fmt.Println("Borre a fede")
-	fmt.Println("Al pedirle al diccionario el valor para fede me da como resultado: ", d.Get("Fede"))
-	fmt.Println("--------------------")
-	fmt.Println("Clave: valor en el diccionario sin fede(String, Int)")
-	for _, key := range d.GetKeys() {
-		fmt.Printf("%v:\t%d\n",key, d.Get(key))
-	}
-	fmt.Println("--------------------")
-	ds := dictionary.NewDictionary[string, set.Set[int]]()
-	s := set.NewSet[int]()
-	s.Add(100)
-	s.Add(222)
-	s.Add(333)
-	ss := set.NewSet[int]()
-	ss.Add(1)
-	ss.Add(2)
-	ss.Add(3)
-	ds.Put("uno", *s)
-	ds.Put("dos", *ss)
-	ds.Put("tres", *ss)
-	fmt.Println("Clave: valor en el diccionario (String, Set[Int])")
-	var aux set.Set[int]
-	for _, key := range ds.GetKeys() {
-		aux = ds.Get(key)
-		fmt.Printf("%v:\t%v\n",key, aux.String())
-	}
-	fmt.Println("--------------------")
-	dl := dictionary.NewDictionary[string, linkedlist.LinkedList[string]]()
-	l:= linkedlist.NewLinkedList[string]()
-	l.InsertAt("A",0)
-	l.InsertAt("B",1)
-	l.InsertAt("C",2)
-	dl.Put("Lista 1",*l)
-	ll:= linkedlist.NewLinkedList[string]()
-	ll.InsertAt("D",0)
-	ll.InsertAt("E",1)
-	ll.InsertAt("F",2)
-	dl.Put("Lista 2", *ll)
-	fmt.Println("Clave: valor en el diccionario (String, LinkedList[String])")
-	var auxl linkedlist.LinkedList[string]
-	for _, key := range dl.GetKeys() {
-		auxl = dl.Get(key)
-		fmt.Printf("%v:\t%v\n",key, auxl.String())
-	}
+	// d := dictionary.NewDictionary[string, int]()
+	// d.Put("Leo", 60)
+	// d.Put("Leo", 61)
+	// d.Put("Fabi", 36)
+	// d.Put("Leo", 60)
+	// d.Put("Fede", 36)
+	// d.Put("Vale", 40)
+	// d.Put("Leo", 50)
+	// fmt.Println("Clave: valor en el diccionario (String, Int)")
+	// for _, key := range d.GetKeys() {
+	// 	fmt.Printf("%v:\t%d\n",key, d.Get(key))
+	// }
+	// fmt.Println("--------------------")
+	// d.Remove("Fede")
+	// fmt.Println("Borre a fede")
+	// fmt.Println("Al pedirle al diccionario el valor para fede me da como resultado: ", d.Get("Fede"))
+	// fmt.Println("--------------------")
+	// fmt.Println("Clave: valor en el diccionario sin fede(String, Int)")
+	// for _, key := range d.GetKeys() {
+	// 	fmt.Printf("%v:\t%d\n",key, d.Get(key))
+	// }
+	// fmt.Println("--------------------")
+	// ds := dictionary.NewDictionary[string, set.Set[int]]()
+	// s := set.NewSet[int]()
+	// s.Add(100)
+	// s.Add(222)
+	// s.Add(333)
+	// ss := set.NewSet[int]()
+	// ss.Add(1)
+	// ss.Add(2)
+	// ss.Add(3)
+	// ds.Put("uno", *s)
+	// ds.Put("dos", *ss)
+	// ds.Put("tres", *ss)
+	// fmt.Println("Clave: valor en el diccionario (String, Set[Int])")
+	// var aux set.Set[int]
+	// for _, key := range ds.GetKeys() {
+	// 	aux = ds.Get(key)
+	// 	fmt.Printf("%v:\t%v\n",key, aux.String())
+	// }
+	// fmt.Println("--------------------")
+	// dl := dictionary.NewDictionary[string, linkedlist.LinkedList[string]]()
+	// l:= linkedlist.NewLinkedList[string]()
+	// l.InsertAt("A",0)
+	// l.InsertAt("B",1)
+	// l.InsertAt("C",2)
+	// dl.Put("Lista 1",*l)
+	// ll:= linkedlist.NewLinkedList[string]()
+	// ll.InsertAt("D",0)
+	// ll.InsertAt("E",1)
+	// ll.InsertAt("F",2)
+	// dl.Put("Lista 2", *ll)
+	// fmt.Println("Clave: valor en el diccionario (String, LinkedList[String])")
+	// var auxl linkedlist.LinkedList[string]
+	// for _, key := range dl.GetKeys() {
+	// 	auxl = dl.Get(key)
+	// 	fmt.Printf("%v:\t%v\n",key, auxl.String())
+	// }
+
+	d := dictionary.NewDictionary[string, []string]()
+	arreglo1 := []string{"Pedro", "Juana"}
+	d.Put("Miercoles", arreglo1)
+	arreglo2 := []string{"Luis", "Pedro", "Juan"}
+	d.Put("Jueves", arreglo2)
+	arreglo3 := []string{"Luis", "Ana", "Juana"}
+	d.Put("Viernes", arreglo3)
+	// arreglo4 := []string{"Lorena", "Luis", "Ana"}
+	// d.Put("Sabado", arreglo4)
+	fmt.Println(d)
+	fmt.Println(ejercicios.InformacionSolicitada(d))
+
 }
